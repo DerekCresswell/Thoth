1488501ae05892e070c9ec14513b26a30c1f1f77
DerekCresswell <derekjcress@gmail.com>
2020-04-17 23:32:07 -0700

    Added documentation to attributes

--- include/Thoth/Attribute.hpp
+++ include/Thoth/Attribute.hpp
@@ -9,38 +9,56 @@
 /*
  *
  * @TODO
- *  Add comments to attribute files
  *  Figure out defining attributes at run time
  *  Implement other common attributes
  *  Expand adding / deleting values to include more convience
  *    Add optional safety for overwriting
- *  Implement other base functions
+ *    Clearing values
+ *    Dealing with empty values
  *  Use conditional_t and is_fundemental_t to increase preformance of
  *  passing dataType by ref or not
  *  Ensure datatype can become string
- *  
- *  Setup Elm to utilise these attributes properly 
- * 
+ *  Organize Attributes by type too
+ *
+ *  Setup Elm to utilise these attributes properly
+ *
  */
 
 namespace Thoth {
-        
+
+    // Forward Declaration for RenderElement
     class RenderElement;
-    
+
     namespace Detail {
 
+        /*
+         *
+         * AttributeBase :
+         * Defines the base feature set that is an attribute that can be attached
+         * to a RenderElement.
+         * Rendering capablities are accessible through the RenderElement.
+         *
+         */
         class AttributeBase {
 
+        // Public variables
         public:
 
+            // The name tof the attribute
+            // I.E. "class", "id"
             const std::string name;
 
+        // Protected functions
         protected:
 
+            // Main constructor for attributes
             AttributeBase(const std::string& name);
 
+            // Returns the attribute as it should be rendered
+            // This should include name, quotation marks, etc.
             virtual std::string Format() = 0;
 
+        // Friends
         public:
 
             friend RenderElement;
@@ -49,61 +67,114 @@ namespace Thoth {
 
     } // namespace Detail
 
+    /*
+     *
+     * StandAloneAttribute :
+     * Base class for an attribute with no values.
+     * By default this will render as 'name' (less quotes).
+     *
+     */
     class StandAloneAttribute : public Detail::AttributeBase {
 
+    // Public functions
     public:
 
+        // Main constructor of StandAloneAttribute
         StandAloneAttribute(const std::string& name);
 
+    // Protected functions
     protected:
 
+        // Override of AttributeBase::Format
         virtual std::string Format() override;
 
     };
 
+    /*
+     *
+     * SingleValueAttribute :
+     * Base class for attributes that have a single data value.
+     * By default it renders as 'name="value"' (less outer quotes).
+     *
+     * The typename 'dataType' determines the type of the value.
+     * This type must be able to convert to a string or have an overridden
+     * 'Format' function.
+     *
+     */
     template<typename dataType>
     class SingleValueAttribute : public Detail::AttributeBase {
 
+    // Public functions
     public:
 
+        // Main constructors for SingleValueAttribute
         SingleValueAttribute(const std::string& name);
         SingleValueAttribute(const std::string& name, const dataType& value);
 
+        // Sets the value of this attribute
         virtual void SetValue(const dataType& value);
 
+    // Protected variables
     protected:
 
+        // The value of this attribute
         dataType value;
 
+    // Protected functions
     protected:
 
+        // Override of AttributeBase::Format
         virtual std::string Format() override;
 
     };
 
+    /*
+     *
+     * MultiValueAttribute :
+     * Base class for attributes with multiple values.
+     * By default renders like 'name=""' (less outer quotes) but requires
+     * 'FormatValue' to put each value into the list.
+     *
+     * The typename 'dataType' determines the type of the value.
+     *
+     */
     template<typename dataType>
     class MultiValueAttribute : public Detail::AttributeBase {
 
+    // Public functions
     public:
 
+        // Main constructors for MultiValueAttribute
         MultiValueAttribute(const std::string& name);
         MultiValueAttribute(const std::string& name, const std::vector<dataType>& values);
 
+        // Sets the values vector directly
+        // Does not perserve previous values
         virtual void SetValues(const std::vector<dataType>& values);
 
+        // Adds a value to this attributes values
+        // Optionally specify a position for it to be put in at (default is last)
         virtual void AddValue(const dataType& value, int position = -1);
 
+        // Removes a value from this attributes values by position or value
+        // Returns a boolean based on whether an element was actually removed
         virtual bool RemoveValue(int position);
         virtual bool RemoveValue(const dataType& value);
 
+    // Protected variables
     protected:
 
+        // Stores the values of this attribute
         std::vector<dataType> values;
 
+    // Protected functions
     protected:
 
+        // Overrides AttributeBase::Format
         virtual std::string Format() override;
 
+        // Defines how an individual value is formatted
+        // and returns it as a string
         virtual std::string FormatValue(const dataType& value) = 0;
 
     };
@@ -118,15 +189,18 @@ namespace Thoth {
 
     /* Public */
 
+    // Constructs an empty SingleValueAttribute
     template<typename dataType>
     SingleValueAttribute<dataType>::SingleValueAttribute(const std::string& name)
         : Detail::AttributeBase(name) {}
 
+    // Constructs a SingleValueAttribute with a value
     template<typename dataType>
     SingleValueAttribute<dataType>::SingleValueAttribute(const std::string& name,
         const dataType& value)
         : Detail::AttributeBase(name), value(value) {}
 
+    // Sets the value of this attribute
     template<typename dataType>
     void SingleValueAttribute<dataType>::SetValue(const dataType& value) {
 
@@ -136,6 +210,8 @@ namespace Thoth {
 
     /* Protected */
 
+    // Default formatting of SingleValueAttibute
+    // 'name="value"'
     template<typename dataType>
     std::string SingleValueAttribute<dataType>::Format() {
 
@@ -147,15 +223,18 @@ namespace Thoth {
 
     /* Public */
 
+    // Constructs an empty MultiValueAttribute
     template<typename dataType>
     MultiValueAttribute<dataType>::MultiValueAttribute(const std::string& name)
         : Detail::AttributeBase(name) {}
 
+    // Constructs MultiValueAttribute with a default value list
     template<typename dataType>
     MultiValueAttribute<dataType>::MultiValueAttribute(const std::string& name,
         const std::vector<dataType>& values)
         : Detail::AttributeBase(name), values(values) {}
 
+    // Directly sets the value list of this attribute
     template<typename dataType>
     void MultiValueAttribute<dataType>::SetValues(const std::vector<dataType>& values) {
 
@@ -163,9 +242,11 @@ namespace Thoth {
 
     }
 
+    // Adds a value to this attributes list
     template<typename dataType>
     void MultiValueAttribute<dataType>::AddValue(const dataType& value, int position) {
 
+        // If position is not set use 'push_back'
         if(position < 0) {
             values.push_back(value);
         } else {
@@ -174,26 +255,31 @@ namespace Thoth {
 
     }
 
+    // Removes a value from this attributes list by position
     template<typename dataType>
     bool MultiValueAttribute<dataType>::RemoveValue(int position) {
 
+        // Ensure the position is valid
         if(position < 0 && position >= values.size())
             return false;
 
-        values.erase(std::remove(values.begin(), values.end(), values[position]), values.end());
+        values.erase(values.begin() + position);
 
         return true;
 
     }
 
-    // @TODO allow removing multiple, + ensure working proper
+    // Removes a value from this attribute by value
+    // @TODO
+    //   allow removing multiple, + ensure working proper
+    //   Add checks for seeing if element was removed
     template<typename dataType>
     bool MultiValueAttribute<dataType>::RemoveValue(const dataType& value) {
 
+        // Try erasing value
         auto it = values.erase(std::remove(values.begin(), values.end(), value), values.end());
 
-        if(it == values.end())
-            return false;
+        // Check if value was found
 
         return true;
 
@@ -201,6 +287,7 @@ namespace Thoth {
 
     /* Protected */
 
+    // Renders this attribute and returns it as a string
     template<typename dataType>
     std::string MultiValueAttribute<dataType>::Format() {
 
@@ -211,6 +298,8 @@ namespace Thoth {
         // Last element must be isolated to prevent the last space being added
         auto lastIt = values.end();
         lastIt--;
+
+        // Loop through every value except the last
         for(auto it = values.begin(); it != lastIt; it++) {
 
             strm << FormatValue(*it) << " ";
--- include/Thoth/Common/Attributes.hpp
+++ include/Thoth/Common/Attributes.hpp
@@ -7,34 +7,66 @@
 
 namespace Thoth {
 
+    /*
+     *
+     * Class :
+     * Defines the 'class' attribute.
+     *
+     */
     class Class : public MultiValueAttribute<std::string> {
 
+    // Public functions
     public:
 
+        // Default constructor
         Class();
 
+    // Protected functions
     protected:
 
+        // Overrides MultiValueAttribute::FormatValue
+        // By default formats as 'class="value"' (less outer quotes)
         virtual std::string FormatValue(const std::string& value) override;
-    
+
     };
 
+    /*
+     *
+     * ID :
+     * Defines the 'id' attribute.
+     *
+     */
     class ID : public SingleValueAttribute<std::string> {
 
+    // Public functions
     public:
 
+        // Default constructor
         ID();
 
     };
 
+    /*
+     *
+     * Style :
+     * Defines the 'style' attribute.
+     * Takes a pair of strings to define each styling where the first value
+     * is the CSS property and the second is the CSS value.
+     *
+     */
     class Style : public MultiValueAttribute<std::pair<std::string, std::string>> {
 
+    // Public functions
     public:
 
+        // Default constructor
         Style();
 
+    // Protected functions
     protected:
 
+        // Overrides MultiValueAttribute::FormatValue
+        // By defaults as 'style="value.first:value.second;"' (Less outer quotes)
         virtual std::string FormatValue(const std::pair<std::string, std::string>& value) override;
 
     };
@@ -49,11 +81,13 @@ namespace Thoth {
 
     /* Public */
 
+    // Constructs an empty class attribute
     Class::Class()
         : MultiValueAttribute<std::string>("class") {}
 
     /* Protected */
 
+    // Formats the values of the class, just passes back value
     std::string Class::FormatValue(const std::string& value) {
 
         return value;
@@ -64,6 +98,7 @@ namespace Thoth {
 
     /* Public */
 
+    // Constructs an empty id attribute
     ID::ID()
         : SingleValueAttribute<std::string>("id") {}
 
@@ -71,11 +106,14 @@ namespace Thoth {
 
     /* Public */
 
+    // Constructs an empty style attribute
     Style::Style()
         : MultiValueAttribute<std::pair<std::string, std::string>>("stlye") {}
 
     /* Protected */
 
+    // Formats the values of a style
+    // Uses 'value.first' as the CSS property and 'value.second' as the CSS value
     std::string Style::FormatValue(const std::pair<std::string, std::string>& value) {
 
         return value.first + ":" + value.second + ";";
